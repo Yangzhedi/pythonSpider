2d4079dcd270ebe90b1e0e2c3148157d3c76dc07
Yangzhedi <uiryzd@163.com>
2016-10-23 16:55:28 +0800

    :tada:  add the count of comment

--- douban/douban_comment.py
+++ douban/douban_comment.py
@@ -17,12 +17,14 @@ headers = {
 data = requests.get(url, headers=headers)
 soup = BeautifulSoup(data.text, 'lxml')
 # users:用户列表    comments:评论列表
+count = soup.select('#content > div > div.article > div.title_line.clearfix.color_gray > span.fleft')
 users = soup.select('div.comment > h3 > span.comment-info > a')
 comments = soup.select('#comments > div.comment-item > div.comment > p')
 
-
+# print count[0].get_text().split('全部共')[1].split('条')[0]
 # print comments[4].get_text().strip().replace('\n',' ')
 f = open('./text.txt','wb')
+f.write(count[0].get_text()+'\n')
 for index,item in enumerate(comments):
     print users[index].get_text() + ':' + item.get_text().strip().strip('\n')+'\n'
     f.write(users[index].get_text() + ':' + item.get_text().strip().replace('\n',' ')+'\n\n')
