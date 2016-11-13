13e8e4ce2635a57ba2d3815ac7c0d21c34429ab0
Yangzhedi <uiryzd@163.com>
2016-11-13 23:07:42 +0800

    完善股票爬虫

--- by-bs4/shares/zhongshiyou.py
+++ by-bs4/shares/zhongshiyou.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import time
+
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.104 Safari/537.36 Core/1.53.1708.400 QQBrowser/9.5.9635.400'
@@ -8,9 +10,14 @@ headers = {
 
 url = 'http://quotes.money.163.com/trade/lsjysj_601857.html?year=2016&season=4'
 
-def sharesCrawl(shareCode,year,month):
 
-    url = 'http://quotes.money.163.com/trade/lsjysj_'+shareCode+'.html?year='+year+'&season='+month
+# parameter
+# shareCode/year/month : num ,
+def sharesCrawl(shareCode,year,month):
+    shareCodeStr = str(shareCode)
+    yearStr = str(year)
+    monthStr = str(month)
+    url = 'http://quotes.money.163.com/trade/lsjysj_'+shareCodeStr+'.html?year='+yearStr+'&season='+monthStr
 
     data = requests.get(url, headers=headers)
     soup = BeautifulSoup(data.text, 'lxml')
@@ -19,11 +26,11 @@ def sharesCrawl(shareCode,year,month):
 
     date = soup.select('div.inner_box > table > tr > td')
 
-    if os.path.exists('./'+shareCode+title) == False:
+    if os.path.exists('./'+shareCodeStr+title) == False:
         #create the share folder
-        os.mkdir('./'+shareCode+title)
+        os.mkdir('./'+shareCodeStr+title)
 
-    f = open('./'+shareCode+title+'/Y'+year+'S'+month+'.txt','wb')
+    f = open('./'+shareCodeStr+title+'/Y'+yearStr+'S'+monthStr+'.txt','wb')
     for index,value in enumerate(date):
         if index % 11 == 10:
             f.write(value.get_text()+'\n')
@@ -31,7 +38,49 @@ def sharesCrawl(shareCode,year,month):
             f.write(value.get_text() +'\t')
     f.close()
 
-sharesCrawl('601600','2016','2')
+# sharesCrawl(600019,'2016','2')
+
+
+
+def sharesCrawl2(shareCode,year,month):
+    shareCodeStr = str(shareCode)
+    yearStr = str(year)
+    monthStr = str(month)
+    url = 'http://quotes.money.163.com/trade/lsjysj_' + shareCodeStr + '.html?year=' + yearStr + '&season=' + monthStr
+    data = requests.get(url, headers=headers)
+    soup = BeautifulSoup(data.text, 'lxml')
+    date = soup.select('div.inner_box > table > tr > td')
+    resultString = ''
+    for index, value in enumerate(date):
+        if index % 11 == 10:
+            resultString += value.get_text() + '\n'
+        else:
+            resultString += value.get_text() + '\t'
+    return resultString
+
+# print sharesCrawl2(600019,2016,2)
+
+
+
+def createUrl(shareCode,beginYear,endYear):
+
+    title = str(shareCode)
+
+    if os.path.exists('./'+title) == False:
+        #create the share folder
+        os.mkdir('./'+title)
+
+    f = open('./' + title + '.txt', 'wb')
+
+    for i in range(beginYear,endYear+1):
+        print i
+        # time.sleep(5)
+        for j in range(1,5):
+            f.write(sharesCrawl2(shareCode,i,j) + '\n ------- '+str(i)+'/'+str(j)+'----------------\n')
+            time.sleep(5)
+    f.close()
+
+createUrl(601857,2008,2016)
 
 
 """
